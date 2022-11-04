2677054f87752f9c993cfe806ae75c7dc7874afb
tomas crovo <tomascrovo17@gmail.com>
2022-11-04 01:38:14 -0300

    agrego funcionalidades de captcha

--- js/script.js
+++ js/script.js
@@ -13,7 +13,45 @@ document.addEventListener("DOMContentLoaded",()=>{
             //aca van las funcionalidades de la seccion sucursales
             break;
         case "contacto":
-            //aca van las funcionalidades de la seccion contacto
+            let operaciones = ["+", "-", "*"];
+            let cuenta = (Math.floor(Math.random()*100)+1)+operaciones[Math.floor(Math.random()*operaciones.length)]+(Math.floor(Math.random()*100)+1);
+            const captcha = document.querySelector("#suma-captcha");
+            captcha.innerHTML = cuenta;
+            const formContacto = document.querySelector("#form-contacto");
+            formContacto.addEventListener("submit",(e)=>{
+                e.preventDefault();
+                let datos = getData(formContacto);
+                const msgError = document.querySelector("#error");
+                console.log(cuenta);
+                if(!datos.gusta){
+                    msgError.innerHTML = "Si no te gusta la cerveza entonces no nos interesa su contacto, que tenga un buen dia"
+                    msgError.classList.remove("message-success");
+                    msgError.classList.add("message-error");
+                }
+                else{
+                    if(datos.nombre && datos.apellido && datos.email && datos.pais && datos.ciudad && datos.consulta && datos.resultado){
+                        console.log(datos.resultado);
+                        if(datos.resultado == eval(cuenta)){
+                            let form = new FormData(formContacto);
+                            enviarMail(formContacto, form);
+                            formContacto.reset();
+                            msgError.innerHTML = "Gracias por contactarte con nosotros, contestaremos lo antes posible";
+                            msgError.classList.remove("message-error");
+                            msgError.classList.add("message-success");
+                        }
+                        else{
+                            msgError.innerHTML = "El captcha esta mal resuelto!";
+                            msgError.classList.remove("message-success");
+                            msgError.classList.add("message-error");    
+                        }
+                    }
+                    else{
+                        msgError.innerHTML = "Algo no salio bien, no intentes romper el formulario por favor";
+                        msgError.classList.remove("message-success");
+                        msgError.classList.add("message-error");
+                    }
+                }
+            })
             break;
         default:
             console.log("este error no deberia ocurrir");
@@ -41,4 +79,39 @@ function odometer(elem){
         inicio = inicio + Math.floor(Math.random()*1000)+50;
         elem.innerHTML = inicio;
     }, 5000);
+}
+
+async function enviarMail(form, data){
+    try{
+        console.log(form.action, form.method)
+        let respuesta = await fetch(form.action, {
+                                    method: form.method,
+                                    body: data,
+                                    headers: {
+                                        'Accept': 'application/json'
+                                    }
+        })
+        if(respuesta.ok){
+            console.log("mensaje enviado");
+        }
+    }
+    catch(error){
+        console.log(error);
+    }
+}
+
+function getData(formulario){
+    let form = new FormData(formulario);
+    let respuesta = {
+        nombre : form.get("nombre"),
+        apellido : form.get("apellido"),
+        telefono : form.get("telefono"),
+        email : form.get("email"),
+        pais : form.get("pais"),
+        ciudad : form.get("ciudad"),
+        gusta : Boolean(form.get("gusta")),
+        consulta : form.get("consulta"),
+        resultado : form.get("captcha")
+    };
+    return respuesta;
 }

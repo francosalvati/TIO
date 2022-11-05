5df98b63c513073f562398a1aa0110b07c6678c3
Ulises <ulipalazzoo@gmail.com>
2022-11-05 11:10:21 -0300

    hago la pagina responsive y arreglo bugs de diseño

--- js/script.js
+++ js/script.js
@@ -1,16 +1,16 @@
 "use strict";
 
+const sucursales = ["Brewery Playa Grande","Brewery Constitución", "Brewery San Telmo", "Brewery Caballito", "Brewery Las Lomitas", "Brewery Campana", "Brewery City Bell", "Brewery Quilmes", "Brewery Berazategui", "Parador Brewery La Plata", "Brewery  La Plata", "Brewery San Isidro", "Brewery Ramos Mejía", "Brewery Bahía Blanca", "Parador Brewery Vicente López", "Brewery Pilar", "Parador Brewery Córdoba", "Brewery Córdoba", "Parador Brewery Villa del Parque", "Brewery Naón", "Brewery Ituzaingó", "Brewery Tucuman", "Brewery Necochea", "Brewery Rosario", "Parador Brewery Villa Urquiza", "Parador Brewery Rosario", "Brewery Calle Olavarria", "Brewery Tandil"];
+
 document.addEventListener("DOMContentLoaded",()=>{
     const url = window.location.pathname.split("/");
     const path = url[url.length-1].slice(0,-5);
     switch(path){
         case"index":
-            //aca van las funcionalidades de la seccion index
             const contador = document.querySelector("#odometer");
             odometer(contador);
             break;
         case"sucursales":
-            //aca van las funcionalidades de la seccion sucursales
             let pagina=1;
             const comentarios = document.querySelector("#idComentarios");
             const paginadoIzquierda = document.querySelector("#pagIzq");
@@ -25,7 +25,7 @@ document.addEventListener("DOMContentLoaded",()=>{
                         }
                     });
                 }
-            }); 
+            });
             const paginadoDerecha = document.querySelector("#pagDer");
             paginadoDerecha.addEventListener("click",()=>{
                 if(pagina<3){
@@ -45,7 +45,7 @@ document.addEventListener("DOMContentLoaded",()=>{
                     comentarios.appendChild(comentario);
                 }
             });
-        break;
+            break;
         case "contacto":
             let operaciones = ["+", "-", "*"];
             let cuenta = (Math.floor(Math.random()*100)+1)+operaciones[Math.floor(Math.random()*operaciones.length)]+(Math.floor(Math.random()*100)+1);
@@ -105,43 +105,6 @@ document.addEventListener("DOMContentLoaded",()=>{
     })
 });
 
-//aca van las funciones de utilidad
-
-function generarComentario(usuario){
-    let card = document.createElement("div");
-    let valoracion = '<i class="fi fi-br-beer"></i>';
-    for(let i=0; i<Math.floor(Math.random() * 3)+2; i++)
-        valoracion+='<i class="fi fi-br-beer"></i>';
-    card.innerHTML = `<div class="card">
-                        <span class="imagen" style="background: url('${usuario.avatar}') no-repeat center/cover;"></span>
-                        <h1>${sucursales[Math.floor(Math.random()*sucursales.length-1)]} <span>@${usuario.first_name}</span></h1>
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque consectetur, quidem laborum illum eius id quisquam vero accusamus dolor aspernatur, officia aliquid! Illum eius dignissimos, doloremque explicabo optio quia rerum!</p>
-                        <span class="valoracion">valoración: ${valoracion}</span>
-                    </div>`
-    return card;
-}
-
-async function getUsuarios(pagina){
-    try{
-        let respuesta = await fetch(`https://reqres.in/api/users?per_page=4&page=${pagina}`);
-        if(respuesta.ok){
-            let arreglo = await respuesta.json();
-            return arreglo;
-        }
-    }catch(error){
-        console.log(error);
-    }
-}
-
-function odometer(elem){
-    let inicio = Math.floor(Math.random()*10000)+1000;
-    elem.innerHTML = inicio;
-    setInterval(() => {
-        inicio = inicio + Math.floor(Math.random()*1000)+50;
-        elem.innerHTML = inicio;
-    }, 5000);
-}
-
 async function enviarMail(form, data){
     try{
         console.log(form.action, form.method)
@@ -175,4 +138,44 @@ function getData(formulario){
         resultado : form.get("captcha")
     };
     return respuesta;
+}
+
+function generarComentario(usuario){
+    let card = document.createElement("div");
+    let valoracion = '<i class="fi fi-br-beer"></i>';
+    for(let i=0; i<Math.floor(Math.random() * 3)+2; i++)
+        valoracion+='<i class="fi fi-br-beer"></i>';
+    card.innerHTML = `<div class="card">
+                        <span class="imagen" style="background: url('${usuario.avatar}') no-repeat center/cover;"></span>
+                        <h1>${sucursales[Math.floor(Math.random()*sucursales.length-1)]} <span>@${usuario.first_name}</span></h1>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque consectetur, quidem laborum illum eius id quisquam vero accusamus dolor aspernatur, officia aliquid! Illum eius dignissimos, doloremque explicabo optio quia rerum!</p>
+                        <span class="valoracion">valoración: ${valoracion}</span>
+                    </div>`
+    return card;
+}
+
+async function getUsuarios(pagina){
+    try{
+        let respuesta = await fetch(`https://reqres.in/api/users?per_page=4&page=${pagina}`);
+        if(respuesta.ok){
+            let arreglo = await respuesta.json();
+            return arreglo;
+        }
+    }catch(error){
+        console.log(error);
+    }
+}
+
+function agregarUsuarios(comentarios, usuarios){
+    console.log(comentarios);
+    console.log(usuarios);
+}
+
+function odometer(elem){
+    let inicio = Math.floor(Math.random()*10000)+1000;
+    elem.innerHTML = inicio;
+    setInterval(() => {
+        inicio = inicio + Math.floor(Math.random()*1000)+50;
+        elem.innerHTML = inicio;
+    }, 5000);
 }
